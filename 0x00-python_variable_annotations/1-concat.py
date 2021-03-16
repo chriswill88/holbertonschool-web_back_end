f423da26fa95a21066a81f1d0f79a43364c1256a
chriswill88 <williechri79@gmail.com>
2021-03-15 22:59:15 -0400

    up to 2

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,5 +2,5 @@
 """ A module for task 1 """
 
 def concat(str1: str, str2: str) -> str:
-    """ concat: concatinates strings """
-    return str(str1 + str2)
+    """ concat: concatinates strings together """
+    return str1 + str2
