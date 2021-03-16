0e33eb61093ec4ba462839d79b77b04ec20a0447
chriswill88 <williechri79@gmail.com>
2021-03-15 22:28:30 -0400

    up to 2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""A module for task 1"""
+
+def concat(str1: str, str2: str) -> str:
+    """concat: concatinates strings"""
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from math import floor as f
+
+
+"""A module for task 2"""
+def floor(n: float) -> int:
+    """floor: calculates the floor for a number"""
+    return f(n)
