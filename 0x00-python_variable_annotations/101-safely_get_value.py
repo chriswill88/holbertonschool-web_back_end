f8fc2e89e58c7278944d0fb0e9423c83067fffc8
chriswill88 <williechri79@gmail.com>
2021-03-16 00:31:43 -0400

    [ADD]

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" This module contains the function for task 100 """
+from typing import List, Any, Sequence, Union, Mapping, TypeVar
+
+
+T = TypeVar('T')
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+    """check_first_element: checks dictionary
+    * Uses TypeVar"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
