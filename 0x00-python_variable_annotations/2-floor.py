c5e684c14eaa1661c84372646a14902c8597d5cc
chriswill88 <williechri79@gmail.com>
2021-03-15 22:57:43 -0400

    up to 2

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -5,6 +5,6 @@
 def floor(n: float) -> int:
     """floor: calculates the floor for a number"""
     if n >= 0:
-        return (n - (n % 1))
+        return int(n - (n % 1))
     else:
-        return (n - (1 - (n % 1)))
+        return int(n - (1 - (n % 1)))
