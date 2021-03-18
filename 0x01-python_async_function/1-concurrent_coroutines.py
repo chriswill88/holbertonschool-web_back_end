8847e6c49c0df85d3c6bd9f83c13639dd750a9cd
chriswill88 <williechri79@gmail.com>
2021-03-17 23:33:31 -0400

    [ADD] all

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+""" This module contains the function for task 1 """
 import asyncio
 from typing import List
 
@@ -7,8 +8,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """this function calls wait_radom in a loop"""
-    ret = []
-    for i in range(n):
-        x = wait_random(max_delay)
-        ret.append(await x)
-    return ret
+    ret = [wait_random(max_delay) for i in range(n)]
+    return [await x for x in asyncio.as_completed(ret)]
