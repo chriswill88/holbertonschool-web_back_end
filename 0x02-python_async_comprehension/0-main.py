ffab52f56a3d6ac34286b602c20eccfe4c053ba1
chriswill88 <williechri79@gmail.com>
2021-03-19 17:20:45 -0400

    [ADD] all

--- 0x02-python_async_comprehension/0-main.py
+++ 0x02-python_async_comprehension/0-main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
 import asyncio
+from typing import Generator
 
 async_generator = __import__('0-async_generator').async_generator
 
-async def print_yielded_values():
+async def print_yielded_values() -> Generator:
     result = []
     async for i in async_generator():
         result.append(i)
