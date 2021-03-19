e5fda973f6f65d209154e675516aa03e341f329a
chriswill88 <williechri79@gmail.com>
2021-03-19 17:14:19 -0400

    [ADD] all

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """ A module for task 0 """
 import random
 import asyncio
+import typing
 
 
-async def async_generator() -> float:
+async def async_generator() -> typing.Union(float, int):
     """async_generator: waits 1 sec then yeilds a random number"""
     for i in range(10):
         await asyncio.sleep(1)
