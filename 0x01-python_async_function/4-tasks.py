69543e2f7380306c95838d135aa68a962a9139b3
chriswill88 <williechri79@gmail.com>
2021-03-19 11:38:16 -0400

    [ADD] all

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" This module contains the function for task 1 """
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """this function calls wait_radom in a loop"""
+    ret = [task_wait_random(max_delay) for i in range(n)]
+    return [await x for x in asyncio.as_completed(ret)]
