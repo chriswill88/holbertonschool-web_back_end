8e073a8f5cfa1e28fc1017e58ae614cdc06bca24
chriswill88 <williechri79@gmail.com>
2021-03-22 21:13:12 -0400

    [ADD]

---
+++ 0x03-caching/2-lifo_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" this is the module for task 0 """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ LIFOCache: limited cache first in first out """
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """ add module to cache """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+        keys = list(self.cache_data.keys())
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            print("DISCARD:", keys[len(keys) - 2])
+            del self.cache_data[keys[len(keys) - 2]]
+
+    def get(self, key):
+        """ return module from cache """
+        cach = self.cache_data
+        return None if key is None or key not in cach.keys() else cach[key]
