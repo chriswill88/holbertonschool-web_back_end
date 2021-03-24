32d311682296e2e6c92b67b3d78f5ed8989cf9b1
chriswill88 <williechri79@gmail.com>
2021-03-24 19:48:52 -0400

    [ADD] all

---
+++ 0x04-pagination/0-simple_helper_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""this module contains the function for task 0"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size:int) -> Tuple:
+    """this returns the start and ending indexs"""
+    return (page - 1, page + page_size - 1)
