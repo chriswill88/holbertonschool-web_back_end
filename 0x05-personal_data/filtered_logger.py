73e7bba7fa5e7fccfb838fc90d820753bf83ac54
chriswill88 <williechri79@gmail.com>
2021-03-29 10:49:27 -0400

    [ADD] all

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -4,8 +4,12 @@ from typing import List
 import re
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(
+    fields: List[str], redaction: str,
+        message: str, separator: str) -> str:
     """classifies message"""
     for i in fields:
-        message = re.sub(r'(?<={}=)(.*?(?={}))'.format(i, separator), redaction, message)
+        message = re.sub(
+            r'(?<={}=)(.*?(?={}))'.format(i, separator),
+            redaction, message)
     return message
