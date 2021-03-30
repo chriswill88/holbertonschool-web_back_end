5aee01281d3c924f726a5b60a6382a808a0b21c1
chriswill88 <williechri79@gmail.com>
2021-03-29 23:52:42 -0400

    [ADD] all

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -6,3 +6,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """hashes a password"""
     return bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """checks a password against a hashed value"""
+    return bcrypt.checkpw(password.encode('utf8'), hashed_password)
