b878ab4ebb6586731f21c1111be569a549566f1c
chriswill88 <williechri79@gmail.com>
2021-04-01 23:58:05 -0400

    [ADD] all

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -45,12 +45,15 @@ def br():
     if auth is None:
         return
 
-    if auth.require_auth(request.path, ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']):
+    if auth.require_auth(
+        request.path, [
+            '/api/v1/status/', '/api/v1/unauthorized/',
+            '/api/v1/forbidden/']):
         pass
 
     if auth.authorization_header(request) is None:
         abort(401)
-    
+
     if auth.current_user(request) is None:
         abort(403)
 
