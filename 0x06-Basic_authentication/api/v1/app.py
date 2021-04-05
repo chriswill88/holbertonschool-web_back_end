90b9d5fe8acffb8b9983b18f655e50b83f2d4eb9
chriswill88 <williechri79@gmail.com>
2021-04-04 22:39:26 -0400

    [ADD] all

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -12,12 +12,12 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = None
-auth = environ["AUTH_TYPE"]
+# auth = None
+# auth = environ["AUTH_TYPE"]
 
-if auth:
-    from api.v1.auth.auth import Auth
-    auth = Auth()
+# if auth:
+#     from api.v1.auth.auth import Auth
+#     auth = Auth()
 
 
 @app.errorhandler(404)
@@ -39,23 +39,23 @@ def forbidden(error) -> str:
     return jsonify({"error": "Forbidden"}), 403
 
 
-@app.before_request()
-def br():
-    """before request"""
-    if auth is None:
-        return
+# @app.before_request()
+# def br():
+#     """before request"""
+#     if auth is None:
+#         return
 
-    if auth.require_auth(
-        request.path, [
-            '/api/v1/status/', '/api/v1/unauthorized/',
-            '/api/v1/forbidden/']):
-        pass
+#     if auth.require_auth(
+#         request.path, [
+#             '/api/v1/status/', '/api/v1/unauthorized/',
+#             '/api/v1/forbidden/']):
+#         pass
 
-    if auth.authorization_header(request) is None:
-        abort(401)
+#     if auth.authorization_header(request) is None:
+#         abort(401)
 
-    if auth.current_user(request) is None:
-        abort(403)
+#     if auth.current_user(request) is None:
+#         abort(403)
 
 
 if __name__ == "__main__":
