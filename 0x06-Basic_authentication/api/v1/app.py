e7d8bfc4a14d94089043dd9641eb3a98be562d05
chriswill88 <williechri79@gmail.com>
2021-04-07 18:57:12 -0400

    [ADD] all

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -49,10 +49,8 @@ def bef():
                 '/api/v1/unauthorized/',
                 '/api/v1/forbidden/']):
             pass
-
         if auth.authorization_header(request) is None:
             abort(401)
-
         if auth.current_user() is None:
             abort(403)
 
