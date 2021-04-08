d396b94a55410b2e5fa56f330cb0c9c02c751826
chriswill88 <williechri79@gmail.com>
2021-04-07 20:51:36 -0400

    [ADD] all

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,15 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Basic Authentication"""
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        if authorization_header is None:
+            return None
+
+        if not isinstance(authorization_header, str):
+            return None
+
+        if authorization_header[0:6] != "Basic ":
+            return None
+
+        return authorization_header[6:]
