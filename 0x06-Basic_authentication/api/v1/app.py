4d7c20d9b7a86377b6bc29795581f5a8295c678b
chriswill88 <williechri79@gmail.com>
2021-04-07 20:12:19 -0400

    [ADD] all

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-auth = environ['AUTH_TYPE']
+auth = getenv('AUTH_TYPE')
 
 if auth:
     from api.v1.auth.auth import Auth
