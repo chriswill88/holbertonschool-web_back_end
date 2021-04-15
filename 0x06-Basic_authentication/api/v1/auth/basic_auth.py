ec2f9730733fad505c2e72e928dcaa3fcb02e284
chriswill88 <williechri79@gmail.com>
2021-04-15 00:13:04 -0400

    [ADD] all

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 This module contains the basic_auth class
 """
 from api.v1.auth.auth import Auth
+from typing import TypeVar
+from models.user import User
 import base64
 
 
@@ -52,3 +54,17 @@ class BasicAuth(Auth):
             return None, None
 
         return decoded_base64_authorization_header.split(':')
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        
+        users = User().search({'email': user_email})
+
+        if len(users)== 0 or not users[0].is_valid_password(user_pwd):
+            return None
+        return users[0]
+        
