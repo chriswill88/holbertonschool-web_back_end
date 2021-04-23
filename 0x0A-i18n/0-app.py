64605abebe84a013cbce34b2048ffb7de7a60fde
chriswill88 <williechri79@gmail.com>
2021-04-22 21:54:53 -0400

    [ADD]

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+"""This module contains task 0"""
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
-"""This module contains task 0"""
+
 
 @app.route('/')
 def hello_world():
