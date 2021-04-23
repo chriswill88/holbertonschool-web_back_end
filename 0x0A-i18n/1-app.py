f1e2d821d5b309a0ebd6f5aba6f8e98c8b4536e7
chriswill88 <williechri79@gmail.com>
2021-04-22 22:37:47 -0400

    [ADD]

---
+++ 0x0A-i18n/1-app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""This module contains task 1"""
+from flask import Flask, render_template
+from flask_babel import Babel
+app = Flask(__name__)
+
+
+class Config():
+    """Config"""
+    LANGUAGES = ["en", "fr"]
+    TIMEZONE = "utc"
+
+babel = Babel(app, Config.LANGUAGES[0], Config.TIMEZONE)
+
+@app.route('/')
+def hello_world():
+    """renders a template"""
+    return render_template('1-index.html')
