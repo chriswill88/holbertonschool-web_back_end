aec28b28eb7eb240f1ef990f4b522123238c5af0
chriswill88 <williechri79@gmail.com>
2021-04-28 20:27:35 -0400

    [ADD]

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 """this module contains SessionAuth class"""
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """SessionAuth class"""
-    pass
+    user_id_by_session_id = {}
+    def create_session(self, user_id: str = None) -> str:
+        """this creates a session"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        user_id[user_id] = uuid.uuid4()
+        return user_id[ user_id]
