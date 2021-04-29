631c001291ed66827ec7b1e7c11418374aca75c7
chriswill88 <williechri79@gmail.com>
2021-04-29 09:23:18 -0400

    [ADD]

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -12,5 +12,5 @@ class SessionAuth(Auth):
         if user_id is None or not isinstance(user_id, str):
             return None
         id = str(uuid.uuid4())
-        self.user_id_by_session_id[user_id] = id
-        return self.user_id_by_session_id[user_id]
+        self.user_id_by_session_id[id] = user_id
+        return id
