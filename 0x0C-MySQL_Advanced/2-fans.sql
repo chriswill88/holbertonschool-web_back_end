5a01f380c3e7a775c6d5c02a00c94b2a1ca4774d
chriswill88 <williechri79@gmail.com>
2021-05-02 10:20:30 -0400

    [ADD]

--- 0x0C-MySQL_Advanced/2-fans.sql
+++ 0x0C-MySQL_Advanced/2-fans.sql
@@ -1,2 +1,2 @@
 -- SQL FOR TASK 2
-SELECT origin, SUM(fans) AS nb_fans FROM holberton.metal_bands GROUP BY origin order by nb_fans DESC;
+SELECT origin, SUM(fans) AS nb_fans FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
