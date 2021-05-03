c10af5fe22f5805047f8ce2fef528750e2d66da9
chriswill88 <williechri79@gmail.com>
2021-05-02 23:18:29 -0400

    [ADD]

--- 0x0C-MySQL_Advanced/5-valid_email.sql
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -1,10 +1,10 @@
 -- TASK 5
 delimiter //
-CREATE TRIGGER email_validator AFTER UPDATE ON users
+CREATE TRIGGER email_validator BEFORE UPDATE ON users
     FOR EACH ROW
     BEGIN
-        IF OLD.email <> NEW.email THEN
-            UPDATE users SET valid_email = 0 WHERE NEW.id = id;
+        IF OLD.email != NEW.email THEN
+            SET NEW.valid_email = 0;
         END IF;
     END;
 //
