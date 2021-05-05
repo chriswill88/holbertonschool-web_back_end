fb9ee6534c27fd365d0b2ac3b0207372e8924849
chriswill88 <williechri79@gmail.com>
2021-05-04 23:59:44 -0400

    [add]

--- 0x0D-NoSQL/other.py
+++ 0x0D-NoSQL/other.py
@@ -5,7 +5,7 @@
 def update_topics(mongo_collection, name, topics):
     """a Python function that changes all topics of
     a school document based on the name"""
-    mongo_collection.update_many(
+    mongo_collection.update_one(
         {'name': name},
         {'$set': {'topics': topics}}
     )
