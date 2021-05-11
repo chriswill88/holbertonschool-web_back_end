8fe4ed8e29d477266e42b68d20059c8347894b6c
chriswill88 <williechri79@gmail.com>
2021-05-11 15:28:43 -0400

    [ADD]

---
+++ 0x0F-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const body = uploadPhoto();
+  const fullname = createUser();
+
+  Promise.all([body, fullname]).then((values) => {
+    console.log(values[0].body, values[1].firstName, values[1].lastName);
+  });
+}
