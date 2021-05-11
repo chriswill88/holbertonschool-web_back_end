d15b9aab70cc63a953bfa508b68c5fc6eda92e5d
chriswill88 <williechri79@gmail.com>
2021-05-11 13:36:21 -0400

    [ADD]

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,16 +1,12 @@
 export default function handleResponseFromAPI(promise) {
-  function res() {
-    return {
-      status: 200,
-      body: 'success',
-    };
-  }
+  promise.then(() => ({
+    status: 200,
+    body: 'success',
+  }));
 
-  function rej() {
-    return Error('');
-  }
+  promise.catch(() => (
+    Error('')
+  ));
 
-  promise.then(res);
-  promise.catch(rej);
   promise.finally(() => console.log('Got a response from the API'));
 }
