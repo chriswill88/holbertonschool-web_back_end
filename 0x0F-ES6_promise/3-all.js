6c69e2ba575366564f6def0f04f4204f7fadadc5
chriswill88 <williechri79@gmail.com>
2021-05-11 15:33:20 -0400

    [ADD]

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -6,5 +6,5 @@ export default function handleProfileSignup() {
 
   Promise.all([body, fullname]).then((values) => {
     console.log(values[0].body, values[1].firstName, values[1].lastName);
-  });
+  }, () => console.log('Signup system offline'));
 }
