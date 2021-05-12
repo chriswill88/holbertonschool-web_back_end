76f5a38059335351b286faecee330880cc41300d
chriswill88 <williechri79@gmail.com>
2021-05-11 20:09:55 -0400

    [ADD]

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,18 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  let body;
-  let fn;
-  let ln;
-
-  return uploadPhoto().then((data) => {
-    body = data.body;
-    createUser().then((data) => {
-      fn = data.firstName;
-      ln = data.lastName;
-      console.log(body, fn, ln);
-    })
-      .catch(() => console.log('Signup system offline'));
-  })
-    .catch(() => console.log('Signup system offline'));
+  return Promise.all([uploadPhoto(), createUser()]).then((values) => {
+    console.log(values[0].body, values[1].firstName, values[1].lastName);
+  }).catch(() => console.log('Signup system offline'));
 }
