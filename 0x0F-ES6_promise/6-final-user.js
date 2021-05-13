08d09fff332b84429f8f9cb43e0db1d18912d477
chriswill88 <williechri79@gmail.com>
2021-05-12 23:24:22 -0400

    [ADD]

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -4,5 +4,8 @@ import signUpUser from './4-user-promise';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const names = signUpUser(firstName, lastName);
   const photo = uploadPhoto(fileName);
-  return Promise.allSettled([names, photo]).then((value) => value, () => 'Error: bob_dylan.jpg cannot be processed');
+  return Promise.allSettled([names, photo]).then((value) => value).catch(() => ({
+    status: 'rejected',
+    value: 'Error: Gerald.jpg cannot be processed',
+  }));
 }
