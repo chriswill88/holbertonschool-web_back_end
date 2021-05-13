c0b9d21121c54355f25cc9e2201dfe826aee2e5e
chriswill88 <williechri79@gmail.com>
2021-05-12 20:32:33 -0400

    [ADD]

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -4,8 +4,7 @@ import signUpUser from './4-user-promise';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const names = signUpUser(firstName, lastName);
   const photo = uploadPhoto(fileName);
-  return Promise.allSettled([names, photo]).finally((data) => {
-    console.log(data);
-    return data;
+  return Promise.allSettled([names, photo]).finally((value) => {
+    return value;
   });
 }
