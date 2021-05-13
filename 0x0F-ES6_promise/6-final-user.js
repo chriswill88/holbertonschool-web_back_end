106320bf0ed8fb99fa25bc02dea0367132ff98f3
chriswill88 <williechri79@gmail.com>
2021-05-13 09:25:35 -0400

    [ADD]

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -3,6 +3,6 @@ import signUpUser from './4-user-promise';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const names = signUpUser(firstName, lastName).then((value) => value);
-  const photo = uploadPhoto(fileName).then((value) => value, (err) => ({ status: 'rejected', value: err.message }));
+  const photo = uploadPhoto(fileName).then((value) => value, (err) => ({ status: 'rejected', value: `Error: ${err.message}` }));
   return Promise.allSettled([names, photo]).then((value) => [value[0], value[1].value]);
 }
