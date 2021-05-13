c34298cddce6e9a0a29c097179457e2ae5e5cc51
chriswill88 <williechri79@gmail.com>
2021-05-12 23:59:41 -0400

    [ADD]

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -2,7 +2,17 @@ import uploadPhoto from './5-photo-reject';
 import signUpUser from './4-user-promise';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const names = signUpUser(firstName, lastName).then((value) => value, (err) => err.value);
-  const photo = uploadPhoto(fileName).then((value) => value, (err) => err.value);
-  return Promise.allSettled([names, photo]);
+  const names = signUpUser(firstName, lastName).then((value) => {
+    return value;
+  }, (err) => {
+    return err.value;
+  });
+  const photo = uploadPhoto(fileName).then((value) => {
+    return value;
+  }, (err) => {
+    return err;
+  });
+  return Promise.allSettled([names, photo]).then((value) => {
+    console.log(value.value);
+  });
 }
