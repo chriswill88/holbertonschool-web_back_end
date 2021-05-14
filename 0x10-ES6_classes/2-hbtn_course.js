44ae2df5912bb95c2931208053961908bbb9743a
chriswill88 <williechri79@gmail.com>
2021-05-13 22:39:33 -0400

    [ADD]

---
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,51 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length;
+
+    if (students.some((s) => typeof s !== 'string')) {
+      throw new TypeError('Students must be a array of strings');
+    }
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  set length(length) {
+    if (typeof length !== 'string') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
+
+  set students(student) {
+    if (typeof student !== 'string') {
+      throw new TypeError('Student must be a string');
+    }
+    this._students.push(student);
+  }
+}
