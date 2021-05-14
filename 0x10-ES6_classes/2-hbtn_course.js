a817726dd9f181ff6e57199a778358f693580903
chriswill88 <williechri79@gmail.com>
2021-05-13 22:58:38 -0400

    [ADD]

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -10,9 +10,10 @@ export default class HolbertonCourse {
     }
     this._length = length;
 
-    // if (typeof students !== 'object') {
-    //   throw new TypeError('Students must be a array of strings');
-    // }
+    if (typeof students !== 'object') {
+      throw new TypeError('Students must be a array of strings');
+    }
+
     // if (students.some((s) => typeof s !== 'string')) {
     //   throw new TypeError('Students must be a array of strings');
     // }
