5b729d082a4da1348af51f834cdba9ebaf5133cb
chriswill88 <williechri79@gmail.com>
2021-05-18 00:05:37 -0400

    [FIX]

--- 0x07-Session_authentication/api/v1/views/session_auth.py
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -6,6 +6,7 @@ from api.v1.views import app_views
 from flask import abort, jsonify, request, session
 from models.user import User
 
+
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def get_email_and_pass():
     """new Flask view that handles all routes for the Session authentication"""
@@ -16,14 +17,11 @@ def get_email_and_pass():
         return jsonify({"error": "email missing"}), 400
     if password is None or password == '':
         return jsonify({"error": "password missing"}), 400
-
     user = User.search({'email': email})[0]
-
     if user is None:
         return jsonify({ "error": "no user found for this email" }), 404
     if user.is_valid_password(password) is False:
         return jsonify({ "error": "wrong password" }), 401
-        
     from api.v1.app import auth
     cookie = getenv('SESSION_NAME')
     session_id = auth.create_session(user.id)
