0285f9dd3520c935ecd4b6435cd3054d0e62749b
chriswill88 <williechri79@gmail.com>
2021-05-18 23:59:04 -0400

    [ADD]

--- 0x11-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,8 @@
 export default function updateStudentGradeByCity(arrObj, city, newGrade) {
   let result = arrObj.filter((stud) => stud.location === city);
 
-  result = result.map((student) => {
+  result = result.map((studenta) => {
+    const student = studenta;
     const res = newGrade.filter((x) => student.id === x.studentId);
     if (res.length > 0) {
       student.grade = res[0].grade;
