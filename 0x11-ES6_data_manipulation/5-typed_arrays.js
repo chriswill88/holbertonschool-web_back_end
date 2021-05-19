ab6af61b511116ab0460357e956b37e6acc62d3f
chriswill88 <williechri79@gmail.com>
2021-05-19 09:09:56 -0400

    [ADD]

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -4,7 +4,7 @@ export default function createInt8TypedArray(length, position, value) {
 
   view.setInt8(position, value);
 
-  if (view.byteLength < position) {
+  if (view.byteLength <= position) {
     throw new Error('Position outside range');
   }
 
