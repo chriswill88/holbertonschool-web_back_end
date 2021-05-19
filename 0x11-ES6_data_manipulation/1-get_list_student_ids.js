ff74939137e8df475ffce56bfc1cd1c1c909de82
chriswill88 <williechri79@gmail.com>
2021-05-18 22:44:54 -0400

    [ADD]

--- 0x11-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x11-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,15 +1,7 @@
 export default function getListStudentIds(arrObj) {
-  let ind = 0;
-  const mymap = new Map();
-
-  if (typeof arrObj !== 'object') {
-    return [];
+  if (arrObj instanceof Array) {
+    const arrId = arrObj.map((x) => x.id);
+    return arrId;
   }
-
-  for (const obj of arrObj) {
-    mymap.set(`ind-${ind}`, obj.id);
-    ind += 1;
-  }
-
-  return Array.from(mymap.values());
+  return [];
 }
