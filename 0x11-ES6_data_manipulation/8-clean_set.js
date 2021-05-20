bfcc2c215cab58a06072251ab77a61f912eab49f
chriswill88 <williechri79@gmail.com>
2021-05-19 23:08:56 -0400

    [ADD]

---
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,21 @@
+export default function cleanSet(set, startString) {
+  const clean = new Set(
+    [...set].filter(
+      (str) => str.substring(0, startString.length) === startString,
+    ),
+  );
+
+  if ([...set].every((x) => clean.has(x))) {
+    return;
+  }
+
+  const result = [...clean].map((x) => x.substring(startString.length));
+
+  let str = '';
+  for (const i of result) {
+    str = str.concat(`${i}-`);
+  }
+
+  str = str.substring(0, str.length - 1);
+  return str;
+}
