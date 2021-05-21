a0e4ac6d581db5006b1e2099ac07eb7596b1a8d1
chriswill88 <williechri79@gmail.com>
2021-05-20 23:23:42 -0400

    [ADD]

---
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -0,0 +1,28 @@
+const csv = require('csv-parser');
+const fs = require('fs');
+
+const students = {};
+let len = 0;
+
+function countStudents(path) {
+  const fileContent = fs.createReadStream(path)
+    .on('error', () => {
+      throw new Error('Cannot load the database');
+    });
+  fileContent.pipe(csv()).on('data', (row) => {
+    len += 1;
+
+    if (!(row.field in students)) {
+      students[row.field] = [];
+    }
+    students[row.field].push(row.firstname);
+    // console.log('stud = ', students);
+  })
+    .on('end', () => {
+      console.log(`Number of students: ${len}`);
+      for (const i of Object.keys(students)) {
+        console.log(`Number of students in ${i}: ${students[i].length}. List: ${students[i].join(', ')}`);
+      }
+    });
+}
+module.exports = countStudents;
