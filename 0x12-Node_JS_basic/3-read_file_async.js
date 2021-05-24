2b99c7c5fc0466c61992d969547ed08eee0f6b9e
chriswill88 <williechri79@gmail.com>
2021-05-23 21:54:16 -0400

    [ADD]

--- 0x12-Node_JS_basic/3-read_file_async.js
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -1,30 +1,31 @@
-const fs = require('fs').promises;
+const fs = require('fs');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
 
 function countStudents(path) {
-  const student = {};
-  let len = 0;
-  let data;
+  return readFile(path, 'utf8').then((data) => {
+    const student = {};
+    let len = 0;
 
-  try {
-    data = fs.readFileSync(path, { encoding: 'utf8', flag: 'r' });
-  } catch (err) {
-    throw new Error('Cannot load the database');
-  }
+    const datalines = data.split('\n');
+    const students = datalines.slice(1).map((line) => line.split(',')).filter((line) => line.length > 0 && line[0] !== '');
 
-  const datalines = data.split('\n');
-  const students = datalines.slice(1).map((line) => line.split(',')).filter((line) => line.length > 0 && line[0] !== '');
-
-  for (const line of students) {
-    len += 1;
-    if (!(line[3] in student)) {
-      student[line[3]] = [];
+    for (const line of students) {
+      len += 1;
+      if (!(line[3] in student)) {
+        student[line[3]] = [];
+      }
+      student[line[3]].push(line[0]);
     }
-    student[line[3]].push(line[0]);
-  }
 
-  console.log(`Number of students: ${len}`);
-  for (const i of Object.keys(student)) {
-    console.log(`Number of students in ${i}: ${student[i].length}. List: ${student[i].join(', ')}`);
-  }
+    console.log(`Number of students: ${len}`);
+    for (const i of Object.keys(student)) {
+      console.log(`Number of students in ${i}: ${student[i].length}. List: ${student[i].join(', ')}`);
+    }
+  }).catch(() => {
+    throw new Error('Cannot load the database');
+  });
 }
+
 module.exports = countStudents;
