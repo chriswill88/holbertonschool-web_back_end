b422c20bb78bba8901566a6d161dfc3a48b5a30c
chriswill88 <williechri79@gmail.com>
2021-05-23 22:33:04 -0400

    [ADD]

--- 0x12-Node_JS_basic/4-http.js
+++ 0x12-Node_JS_basic/4-http.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.write('Hello Holberton School!');
+  res.end('Hello Holberton School!');
 }).listen(1245, 'localhost');
 
 exports.app = app;
