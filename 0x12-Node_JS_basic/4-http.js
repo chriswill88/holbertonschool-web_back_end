bb39d37cedd7954a5447135b6cfae9e9392580c9
chriswill88 <williechri79@gmail.com>
2021-05-23 22:04:45 -0400

    [ADD]

---
+++ 0x12-Node_JS_basic/4-http.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const host = 'localhost';
+const port = 1245;
+
+const requestListener = (req, res) => {
+  res.writeHead(200);
+  res.end('Hello Holberton School!');
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host);
