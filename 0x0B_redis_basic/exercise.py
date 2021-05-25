89338f078b034296d2f42e9da563e6ee0a446f63
chriswill88 <williechri79@gmail.com>
2021-05-25 16:45:16 -0400

    [ADD]

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,6 +2,7 @@
 """This is the module for exercize.py"""
 import redis
 import uuid
+from typing import Union
 
 
 class Cache:
@@ -10,7 +11,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
     
-    def store(self, data) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """this function stores a variable in redis"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
