93452b29ddb6293eb4299db886dcd388a2c353d5
chriswill88 <williechri79@gmail.com>
2021-05-25 16:40:18 -0400

    [ADD]

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -10,7 +10,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
     
-    def store(self, data ):
+    def store(self, data: str) -> str:
         """this function stores a variable in redis"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
