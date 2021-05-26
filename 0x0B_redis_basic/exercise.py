e3e072faff16a1f523f3399203a487e94068926d
chriswill88 <williechri79@gmail.com>
2021-05-25 22:34:23 -0400

    [ADD]

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -6,12 +6,24 @@ from typing import Union, Callable
 from functools import wraps
 
 
+def call_history(method: Callable) -> Callable:
+    """wrapper"""
+    @wraps(method)
+    def wrapper(self, *args):
+        """This function creates a history of input and output for Cache.store"""
+        key = method.__qualname__
+        self._redis.rpush(key + ':inputs', str(args))
+        self._redis.rpush(key + ':outputs', str(method(self, args)))
+        return method(self, args)
+
+
 def count_calls(method: Callable) -> Callable:
     """wrapper"""
     @wraps(method)
     def wrapper(self, *args):
-        """this function counts the number of times store is called"""
+        """this function counts the number of times Cache.store is called"""
         key = method.__qualname__
+        print(key)
         self._redis.incr(key, 1)
         return method(self, *args)
     return wrapper
@@ -21,14 +33,15 @@ class Cache:
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
-    
-    @count_calls
+
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """this function stores a variable in redis"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
+    @count_calls
+    @call_history
     def get(self, key: str, fn=None):
         """this function converts the """
         value = self._redis.get(key)
