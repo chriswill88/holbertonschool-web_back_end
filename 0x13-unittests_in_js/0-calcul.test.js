354129605119deb6893e5864efcd0cf35bc8810a
chriswill88 <williechri79@gmail.com>
2021-05-26 21:52:27 -0400

    [ADD]

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,23 +1,20 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function() {
+describe('calculateNumber right', function() {
   it('...', function() {
     assert.equal(calculateNumber(1, 3), 4);
   });
   it('...', function() {
     assert.equal(calculateNumber(1, 3.7), 5);
   });
+});
+
+describe('calculateNumber left', function() {
   it('...', function() {
-    assert.equal(calculateNumber(1.2, 3.7), 5);
+    assert.equal(calculateNumber(1, 3), 4);
   });
   it('...', function() {
-    assert.equal(calculateNumber(1.5, 3.7), 6);
-  });
-  it('...', function() {
-    assert.equal(calculateNumber(0, 3.7), 4);
-  });
-  it('...', function() {
-    assert.equal(calculateNumber(0, 0), 0);
+    assert.equal(calculateNumber(1.2, 3), 4);
   });
 });
