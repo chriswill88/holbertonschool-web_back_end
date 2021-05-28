05fd2072ce1977fe434174f4725d2a015d2e334e
chriswill88 <williechri79@gmail.com>
2021-05-28 02:17:12 -0400

    [ADD]

--- 0x13-unittests_in_js/3-payment.test.js
+++ 0x13-unittests_in_js/3-payment.test.js
@@ -6,9 +6,7 @@ const expect = require('chai').expect;
 
 describe('test', function() {
   it('...', function() {
-    const sandbox = sinon.createSandbox();
-
-    const spy = sandbox.spy(Utils, 'calculateNumber');
+    const spy = sinon.spy(Utils, 'calculateNumber');
     sendPaymentRequestToApi(100, 20);
 
     expect(spy.calledWith('SUM', 100, 20)).to.be.true;
