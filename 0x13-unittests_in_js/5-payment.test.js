1b4e200e4d7ea5934923731de93d30b264b672f5
chriswill88 <williechri79@gmail.com>
2021-05-28 20:04:22 -0400

    [ADD]

---
+++ 0x13-unittests_in_js/5-payment.test.js
@@ -0,0 +1,33 @@
+const expect = require('chai').expect;
+const assert = require('chai').assert;
+
+const sendPaymentRequestToApi = require('./4-payment');
+const sinon = require('sinon');
+
+
+
+describe('test', function() {
+  const sandbox = sinon.createSandbox();
+
+  this.beforeEach(function() {
+    sandbox.spy(console, 'log');
+  });
+
+  this.afterEach(function() {
+    sandbox.restore();
+  });
+
+  it('...', function() {
+    sendPaymentRequestToApi(100, 20);
+
+    assert(console.log.calledOnce);
+    assert(console.log.calledWith('The total is: 120'));
+  });
+
+  it('...', function() {
+    sendPaymentRequestToApi(10, 10);
+
+    assert(console.log.calledWith('The total is: 20'));
+    assert(console.log.calledOnce);
+  });
+});
