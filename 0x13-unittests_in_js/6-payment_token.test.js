9fe07204493980d2e202bd00b8cdd5c2cd94b12f
chriswill88 <williechri79@gmail.com>
2021-05-30 02:44:17 -0400

    [ADD]

--- 0x13-unittests_in_js/6-payment_token.test.js
+++ 0x13-unittests_in_js/6-payment_token.test.js
@@ -1,13 +1,18 @@
 getPaymentTokenFromAPI = require('./6-payment_token');
 const assert = require('chai').assert;
+const expect = require('chai').expect;
 const sinon = require('sinon');
 
 
-describe('getPaymentTokenFromAPI', function() {
-  it('getPaymentTokenFromAPI', function(done) {
-    spy = sinon.spy(getPaymentTokenFromAPI);
-    console.log(getPaymentTokenFromAPI(true));
-    assert(spy.returned);
-    done();
+describe('getPaymentTokenFromAPI', async function() {
+  it('getPaymentTokenFromAPI', async function() {
+    sinon.spy(console, 'log');
+    const promise = getPaymentTokenFromAPI(true);
+    const result = await promise;
+    const obj = { data: 'Successful response from the API' };
+
+    console.log(result);
+
+    expect(console.log.calledWith(obj)).to.be.true;
   });
 });
