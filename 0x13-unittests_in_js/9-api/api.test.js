7cf1f94382bb7536eeac3765650815a2e5adf37c
chriswill88 <williechri79@gmail.com>
2021-05-31 08:21:22 -0400

    [ADD]

--- 0x13-unittests_in_js/9-api/api.test.js
+++ 0x13-unittests_in_js/9-api/api.test.js
@@ -4,38 +4,43 @@ const { spy } = require('sinon');
 
 
 describe('index page', () => {
-  spy(console, 'log');
+  beforeEach(() => {
+    this.get = stub(request, 'get');
+    this.post = stub(request, 'post');
+  });
+
+  afterEach(() => {
+    request.get.restore();
+    request.post.restore();
+
+  });
 
   it('status code', () => {
     request('http://localhost:7865/cart/12', function(err, res, body) {
-      // console.log(res.toJSON().request.uri.path.split('/')[2]);
-      console.log(res.statusCode)
-      expect(console.log.calledWith(200)).to.be.true;
+      expect(res.statusCode).to.equal(200);
     });
   });
   it('status code', () => {
     request('http://localhost:7865/cart/hello', function(err, res, body) {
-      // console.log(res.toJSON().request.uri.path.split('/')[2]);
-      console.log(res.statusCode)
-      expect(console.log.calledWith(404)).to.be.true;
+      expect(res.statusCode).to.equal(404);
     });
   });
-  it('body returns', () => {
+  it('body returns 1', () => {
     request('http://localhost:7865/', function(err, res, body) {
-      console.log(body);
-      expect(console.log.calledWith('Welcome to the payment system')).to.be.true;
+      expect(body).to.equal('Welcome to the payment system');
     });
   });
-  it('body returns', () => {
+
+
+  it('body returns 2', () => {
     request('http://localhost:7865/cart/12', function(err, res, body) {
-      console.log(body);
-      expect(console.log.calledWith('Payment methods for cart 12')).to.be.true;
+      expect(body).to.equal('Payment methods for cart 12');
     });
   });
-  it('get', () => {
+
+  it('get?', () => {
     request('http://localhost:7865/', function(err, res, body) {
-      console.log(res.request.method);
-      expect(console.log.calledWith('GET')).to.be.true;
+      expect(res.request.method).to.equal('GET');
     });
   });
 });
