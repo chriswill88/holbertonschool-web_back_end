efe630392bac6b43e566a9e90b25c92c0072b24c
chriswill88 <williechri79@gmail.com>
2021-06-03 20:15:36 -0400

    [add]

--- 0x14-queuing_system_in_js/6-job_creator.js
+++ 0x14-queuing_system_in_js/6-job_creator.js
@@ -2,8 +2,8 @@ const kue = require('kue')
 const queue = kue.createQueue();
 
 const jobject = {
-  phoneNumber: '2039992838',
-  message: 'the message',
+  phoneNumber: '4153518780',
+  message: 'This is the code to verify your account',
 }
 
 const job = queue.create('push_notification_code', jobject).save()
