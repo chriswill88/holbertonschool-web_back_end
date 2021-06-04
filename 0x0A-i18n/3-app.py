4155f5bf0103bbb63069aa398514d4d2f7f2e607
chriswill88 <williechri79@gmail.com>
2021-06-04 05:13:55 -0400

    [ADD]

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""This module contains task 1"""
+"""This module contains task 3"""
 from flask import Flask, render_template, request
 from flask_babel import Babel, gettext
 
 
 class Config():
-    """Config"""
+    """Configuration class for templates"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -18,11 +18,13 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
-    """lang selector"""
+    """lang selector based on location"""
     return request.accept_languages.best_match(Config.LANGUAGES)
 
 
 @app.route('/')
 def hello_world():
-    """renders a template"""
-    return render_template('3-index.html')
+    """This function renders a template"""
+    return render_template(
+        '3-index.html', home_title=gettext(u'home_title'),
+        home_header=gettext(u'home_header'))
